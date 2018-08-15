ca3200f7148e8108c584a9b54ff3d8c44d59f4e8
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-08-15 16:01:47 -0300

    commit 4

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -28,5 +28,31 @@ $(document).ready(function() {
       var errorMessage = error.message;
       alert(errorCode, errorMessage);
     });
-  })
+  });
+
+  $(".sign-in-button").click(function(event) {
+     event.preventDefault();
+
+     var email = $(".sign-in-email").val();
+     var name = $(".sign-in-name").val();
+     var password = $(".sign-in-password").val();
+
+     firebase.auth().signInWithEmailAndPassword(email, password)
+     .then(function(response) {
+       var userId = response.user.uid;
+
+       database.ref("users/" + userId).set({
+         name: name,
+         email: email
+       });
+
+       window.location = "posts.html?userId=" + userId
+
+     })
+     .catch(function(error) {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       alert(errorCode, errorMessage);
+     });
+   });
 });
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,7 +1,6 @@
 var database = firebase.database();
 var USER_ID = window.location.search.match(/\?userId=(.*)/)[1];
 
-
 $(document).ready(function() {
   database.ref("users/" + USER_ID).once("value")
   .then(function(snapshot) {
@@ -83,53 +82,3 @@ function createUsers(name, key) {
   })
 
 }
-
-
-
-// var database = firebase.database();
-// var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
-//
-// $(document).ready(function() {
-//   getPostsFromDB();
-//   $(".add-posts").click(addPostsClick);
-// });
-//
-// function addPostsClick(event) {
-//   event.preventDefault();
-//
-//   var newPost = $(".posts-input").val();
-//   var visualization = $("#visualization option:selected").val(); //
-//   var postFromDB = addPostToDB(newPost);
-//
-//   createListItem(newPost, postFromDB.key)
-// }
-//
-// function addPostToDB(text, visualization) {
-//   return database.ref("posts/" + USER_ID).push({
-//     text: text
-//   });
-// }
-//
-// function getPostsFromDB() {
-//   database.ref("posts/" + USER_ID).once('value')
-//   .then(function(snapshot) {
-//     snapshot.forEach(function(childSnapshot) {
-//       var childKey = childSnapshot.key;
-//       var childData = childSnapshot.val();
-//       createListItem(childData.text, childKey)
-//     });
-//   });
-// }
-//
-// function createListItem(text, key) {
-//   $(".posts-list").append(`
-//     <li>
-//     <input type="checkbox" data-post-id=${key} />
-//     <span>${text}</span>
-//     </li>`);
-//
-//     $(`input[data-post-id="${key}"]`).click(function() {
-//       database.ref("posts/" + USER_ID + "/" + key).remove();
-//       $(this).parent().remove();
-//     });
-//   }
